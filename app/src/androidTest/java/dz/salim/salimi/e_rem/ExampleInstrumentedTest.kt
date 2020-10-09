74cb32e751e9db40ae166a70d9a4dff8e697c332
Salim Salimi <salim.salimi@protonmail.com>
2020-10-09 12:46:30 +0200

    Initial Commit!

---
+++ app/src/androidTest/java/dz/salim/salimi/e_rem/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package dz.salim.salimi.e_rem
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("dz.salim.salimi.e_rem", appContext.packageName)
+    }
+}
