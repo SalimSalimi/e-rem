a68e2211e2b51abf4751da316b50f6217ae5a2c0
Salim Salimi <salim.salimi@protonmail.com>
2020-10-10 23:21:38 +0200

    Creating a RecyclerView for showing list of courses

--- app/src/main/java/dz/salim/salimi/e_rem/ui/courselist/CourseListViewModel.kt
+++ app/src/main/java/dz/salim/salimi/e_rem/ui/courselist/CourseListViewModel.kt
@@ -1,4 +1,4 @@
-package dz.salim.salimi.e_rem.ui.listcourse
+package dz.salim.salimi.e_rem.ui.courselist
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
@@ -6,7 +6,7 @@ import androidx.lifecycle.ViewModel
 import dz.salim.salimi.e_rem.data.models.Course
 import dz.salim.salimi.e_rem.data.repositories.CourseRepository
 
-class ListCourseViewModel: ViewModel() {
+class CourseListViewModel: ViewModel() {
 
     private val _listCourses = MutableLiveData<List<Course>>()
     val listCourses : LiveData<List<Course>>
