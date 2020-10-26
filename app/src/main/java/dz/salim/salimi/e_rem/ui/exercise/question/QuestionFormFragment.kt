0f0ffca8c55cec406465d4ce3b0616deac198faf
Salim Salimi <salim.salimi@protonmail.com>
2020-10-26 14:11:38 +0100

    enhacement: Create questions and add options using a dialog

--- app/src/main/java/dz/salim/salimi/e_rem/ui/exercise/question/QuestionFormFragment.kt
+++ app/src/main/java/dz/salim/salimi/e_rem/ui/exercise/question/QuestionFormFragment.kt
@@ -1,14 +1,23 @@
 package dz.salim.salimi.e_rem.ui.exercise.question
 
+import android.app.Dialog
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.DialogFragment
+import com.google.android.material.checkbox.MaterialCheckBox
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textview.MaterialTextView
 import dz.salim.salimi.e_rem.R
 import dz.salim.salimi.e_rem.databinding.QuestionFormFragmentBinding
+import kotlinx.android.synthetic.main.dialog_custom.*
 
 class QuestionFormFragment : Fragment() {
 
@@ -27,6 +36,29 @@ class QuestionFormFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(QuestionFormViewModel::class.java)
         binding.questionVm = viewModel
+        binding.addOptionBtn.setOnClickListener {
+            alert()
+        }
     }
 
+    private fun alert() {
+        val view = layoutInflater.inflate(R.layout.dialog_custom, null, false)
+        val optionEditText = view.findViewById<TextInputEditText>(R.id.option_title_edit_input)
+        val isCorrectCheckBox = view.findViewById<MaterialCheckBox>(R.id.is_correct_check)
+
+        MaterialAlertDialogBuilder(requireContext())
+            .setTitle(resources.getString(R.string.add_an_option))
+            .setMessage("Insert a new option and check the box if it's true or not!")
+            .setView(view)
+            .setPositiveButton(resources.getString(R.string.confirm)){ _, _ ->
+                val option = optionEditText.text.toString()
+                val isCorrect = isCorrectCheckBox.isChecked
+
+                viewModel.addOptions(option, isCorrect)
+            }
+            .setNegativeButton(resources.getString(R.string.cancel)){ dialog, _ ->
+                dialog.cancel()
+            }
+            .show()
+    }
 }
--- app/src/main/java/dz/salim/salimi/e_rem/ui/exercise/question/QuestionFormViewModel.kt
+++ app/src/main/java/dz/salim/salimi/e_rem/ui/exercise/question/QuestionFormViewModel.kt
@@ -4,7 +4,11 @@ import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import dz.salim.salimi.e_rem.data.models.content.Exercise
 import dz.salim.salimi.e_rem.data.models.content.Question
+import dz.salim.salimi.e_rem.data.repositories.AuthRepository
+import dz.salim.salimi.e_rem.data.repositories.ExerciseRepository
+import dz.salim.salimi.e_rem.utils.getCurrentTime
 
 class QuestionFormViewModel : ViewModel() {
     private val _question = MutableLiveData<Question>()
@@ -13,14 +17,29 @@ class QuestionFormViewModel : ViewModel() {
 
     val pointsString = MutableLiveData<String>()
 
+    private val _hashOptions = MutableLiveData<HashMap<String, Boolean>>()
 
     init {
         _question.value = Question()
         pointsString.value = "0"
+        _hashOptions.value = HashMap()
     }
 
     fun onAddOptionsClick() {
-        _question.value!!.points = pointsString.value.toString().toInt() ?: 0
+        _question.value!!.points = pointsString.value?.toString()?.toInt() ?: 0
         Log.d("ViewModel", "points= ${_question.value!!.points}")
+    }
+
+    fun addOptions(option: String, isCorrect: Boolean) {
+        _hashOptions.value!![option] = isCorrect
+    }
+
+    fun onAddQuestionClicked() {
+        val questionsList = ArrayList<Question>()
+        _question.value!!.options = _hashOptions.value!!
+        questionsList.add(_question.value!!)
+        val exercise = Exercise("", "Hello", "World", getCurrentTime(),
+            "", AuthRepository.LOGGED_IN_USER_UID, questionsList)
+        ExerciseRepository.addExercise(exercise)
     }
 }
